d3e347745ea44ef1bbbe90ae2ee507e4dac2c0a4
Chakrapani Kurra <chucky@Chakrapanis-MacBook-Air.local>
2023-02-06 11:35:05 -0700

    completed project

---
+++ feedback.php
@@ -0,0 +1,38 @@
+<?php include 'inc/header.php'; ?>
+
+
+<?php 
+
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
+
+
+   
+    <h2> PAST Feedback</h2>
+
+
+    <?php  
+
+    if(empty($feedback)){ 
+      echo "Feedback empty";
+    }
+    
+    ?>
+
+<?php foreach ($feedback as $item): ?>
+  <div class="card my-3 w-75">
+  <div class="card-body text-center">
+    <?php echo ($item['body']); ?>
+    <div class = "text-secondary mt-2" >
+     By <?php echo $item['name']; ?>
+    </div>
+</div>
+</div>
+
+  
+<?php endforeach; ?>
+
+   <?php include 'inc/footer.php'; ?>
